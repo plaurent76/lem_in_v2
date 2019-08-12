494afb9e021d57484311671b1c7c1061d16fb518
Paul de Renty <contact@paul-r.com>
2019-08-12 12:57:58 +0200

    cc

--- new_bfs.c
+++ new_bfs.c
@@ -75,15 +75,16 @@ int		room_used(int **mx, int path_n, int id)
 	length = 0;
 	while (mx[path_n][length] != -1)
 	{
-		ft_printf("")
 		if (mx[path_n][length] == id)
 		{
-			ft_printf("room %d found in path %d\n", id, path_n);
+			ft_printf("room %d found in path %d: ", id, path_n);
+			print_path(mx[path_n], length);
 			return (1);
 		}
 		length++;
 	}
-	ft_printf("room %d NOT found in path %d\n", id, path_n);
+	ft_printf("room %d NOT found in path %d: ", id, path_n);
+	print_path(mx[path_n], length);
 	if (path_n > 0) {
 		//ft_printf("path_n: %d length: %d\n", path_n, length);
 		if (paths_match(mx[path_n], mx[path_n - 1], length)
